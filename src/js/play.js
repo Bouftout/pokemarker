fd0743d2e3f737ba55f037fe622e0b637a622c7e
ToniPortal <tonitoctoc13@gmail.com>
2023-03-14 17:29:49 +0100

    envoie

--- src/js/play.js
+++ src/js/play.js
@@ -3,180 +3,24 @@ window.onload = function () {
     const loc = location.origin; // Avoir l'adresse du site sans /
     var socket = io(`${loc}`); // Connexion au serveur
 
+    var username = document.getElementById('usernames').innerText; // Get le span ou est situé ton username de base : grâce a l'ejs.
+    quelpokemonatu(username) // Start la fonction de quel pokemon tu dispose par rapport a ton username
 
-    var input = document.getElementById('inputroom');
-    var username = document.getElementById('usernames').innerText;
-    const err = document.getElementById("err");
-    quelpokemonatu(username);
+    //Vitesse est une chose pour voir lequel commence son tour
+    // (Force +evforce) - (Defense + evDefense) 
+    let nbjoueurs = document.getElementById("nbjoueurs");    
+    socket.emit('connectpoke')
 
-    document.getElementById("joinroom").addEventListener('click', function (e) {
-        e.preventDefault();
-        if (input.value.length > 0 && input.value) {
-            try {
-                socket.emit('connectpoke', username, input.value);
-                envoie(username);
-            } catch (e) {
-                console.log(e);
-            } finally {
-                document.getElementById("choixpokemon").setAttribute("style", "display: none");
-
-                err.innerText = `Vous allez rejoindre la room ${input.value}`;
-            }
-
-        }
-    });
-
-    document.getElementById("quitroom").addEventListener('click', function (e) {
-        e.preventDefault();
-        if (input.value.length > 0 && input.value) {
-            try {
-                socket.emit('discopoke', username, input.value);
-            } catch (e) {
-                console.log(e);
-            } finally {
-                err.innerText = `Vous allez quittez la room ${input.value}`;
-            }
-
-        }
-    });
-
-
-    socket.on('connectpokenew', function (name, room) {
-        let nbjoueurs = document.getElementById("nbjoueurs");
-        nbjoueursinner = nbjoueurs.innerText;
-        if (room && name) {
-            nbjoueurs.innerText = Number(nbjoueurs.innerText) + Number(1);
-            if (nbjoueursinner == 0) {
-                err.innerText = `Vous avez rejoind la room ${room}`;
-                err.style.color = "grey";
-            } else {
-                err.innerText = `${name} a rejoint la room ${room}`;
-                err.style.color = "green";
-            }
-        }
-    });
-
-    var refreshIntervalId = setInterval(function () {
-        let nbjoueurs = document.getElementById("nbjoueurs");
-        nbjoueursinner = nbjoueurs.innerText;
-        if (nbjoueursinner == 2) {
-            document.getElementById("rejoin").style.display = "none"
-            clearInterval(refreshIntervalId);
-            socket.emit('retablirjoueursserv', input.value, username);
-        }
-    }, 1000);
-
-    socket.on('retablirjoueursclient', function (name) {
-        let nbjoueurs = document.getElementById("nbjoueurs");
-        nbjoueursinner = nbjoueurs.innerText;
-
-
-        if (nbjoueursinner != 2) {
-            nbjoueurs.innerText = Number(nbjoueurs.innerText) + Number(1);
-            err.innerText = `Vous avez rejoind la room ${input.value} avec ${name}`;
-            err.style.color = "green";
-        }
+    socket.on('newplayer', function (nbplayer) {
+        console.log(nbplayer)
+        nbjoueurs.innerText = nbplayer;
 
     });
 
 
+}
 
 
-    socket.on('discopokenew', function (name, room) {
-        let nbjoueurs = document.getElementById("nbjoueurs");
-        nbjoueursinner = nbjoueurs.innerText;
-        if (room && name) {
-            nbjoueurs.innerText = Number(nbjoueurs.innerText) - Number(1);
-            if (nbjoueursinner == 0) {
-                err.innerText = `Vous avez quittez ${room}`;
-            } else {
-                err.innerText = `${name} a quittez la room ${room}`;
-            }
-        }
-    });
-
-    var envoisi2player = setInterval(function () {
-        let nbjoueurs = document.getElementById("nbjoueurs");
-        nbjoueursinner = nbjoueurs.innerText;
-        if (nbjoueursinner == 2) {
-            var select = document.getElementById('pokemon');
-            var valsel = select.options[select.selectedIndex].value;
-            socket.emit('envoisi2player', input.value, username, valsel);
-        }
-    }, 100);
-
-    var lefairequeunefois = true;
-
-    socket.on('envoiepokemon', function (name, valpokemon) {
-        clearInterval(envoisi2player);
-        if (name != username && lefairequeunefois == true) {
-
-            envoiepokemon2(valpokemon);
-            lefairequeunefois = false;
-            var select = document.getElementById('pokemon');
-            var valsel = select.options[select.selectedIndex].value;
-            socket.emit('envoisi2player', input.value, username, valsel);
-
-        }
-    });
-
-    var combatfirst = 0;
-
-    socket.on('qqalancer', function (name, nompokemon, vitesse) {
-        const err = document.getElementById("err");
-        console.log(name)
-        console.log(combatfirst)
-        if (combatfirst == 0) {
-            combatfirst++;
-
-            const p2username = document.getElementById("p2username").innerText;
-            const p1vitesse = document.getElementById("p1vitesse").innerText;
-            const p2vitesse = document.getElementById("p2vitesse").innerText;
-
-
-            if (name == username && p1vitesse <= vitesse) {
-                document.getElementById("combat").style.display = "none";
-            } else if (name != username && p1vitesse <= vitesse) {
-                document.getElementById("combat").style.display = "none";
-            }
-
-            if (name == username && p1vitesse > vitesse) {
-                document.getElementById("combat").style.display = "block";
-            } else if (name != username && p1vitesse > vitesse) {
-                document.getElementById("combat").style.display = "block";
-            }
-        }
-
-
-    });
-
-    socket.on('btncmb', function (name,nompokemon) {
-        console.log("btncmb")
-        const p2username = document.getElementById("p2username").innerText;
-        if (name == username) {
-            err.innerText = (`Vous avez lancé le combat ${name} avec votre pokemon ${nompokemon} VS ${p2username}`);
-            err.style.color = "grey";
-            socket.emit('prendredgt', input.value, name, );
-        } else if (name != username) {
-            err.innerText = (`${name} a lancé le combat avec son pokemon ${nompokemon} contre vous ${username}`);
-            err.style.color = "grey";
-        } else {
-            alert("Oups, il y a un problème");
-        }
-
-    });
-
-    socket.on('prendredgt', function (name,degat) {
-
-        if(name != username){
-            document.getElementById("p2pv").innerText = Number(document.getElementById("p2pv").innerText) - Number(degat);
-        }
-
-    })
-
-
-
-};
 
 async function quelpokemonatu(usernamefunc) {
 
@@ -188,28 +32,37 @@ async function quelpokemonatu(usernamefunc) {
             'Content-Type': 'application/json',
         }
     };
-    const response = await fetch(`${loc}/get/${usernamefunc}/pokemon`, settings); // Requête
-    if (response.status >= 200 && response.status <= 299) {
-        let log = await response.json();
-        console.log(log)
-        if (log) {
-            let select = document.createElement("select");
-            select.setAttribute("id", "pokemon");
-            select.setAttribute("name", "pokemon");
-            for (let i = 0; i < log.length; i++) {
+    const response = await fetch(`${loc}/get/${usernamefunc}/pokemon`, settings); // Requête qui donne par rapport a un nom tout les pokemon a ça disposition
+    if (response.status >= 200 && response.status <= 299) { // Si la requete il n'y a pas d'erreur
+
+        let data = await response.json(); //Le retour json de la requete
+
+        if (data) {
+
+            let select = document.createElement("select"); //Création d'un select
+            select.setAttribute("id", "pokemon"); // Set id a "pokemon"
+            select.setAttribute("name", "pokemon"); // Set name a "pokemon"
+            for (let i = 0; i < data.length; i++) { // Boucle qui se fait par le nombre de pokemon a disposition dans le "data"
+
                 let option = document.createElement("option");
-                option.setAttribute("value", log[i].givenname);
-                option.innerText = log[i].givenname;
+                option.setAttribute("value", data[i].givenname);
+                option.innerText = data[i].givenname;
                 select.appendChild(option);
+
             }
-            document.getElementById("choixpokemon").appendChild(select);
+
+            document.getElementById("choixpokemon").appendChild(select); //Ajout a la div "choixpokemon" le select qui dispose tout les pokemon du joueurs
+
+            //Crée pour confirmer le choix
+
+
         } else {
             document.getElementById("err").innerText = "Vous n'avez pas de pokemon";
             document.getElementById("err").style.color = "red";
         }
 
     } else {
-        // Handle errors
+        // Si erreur niveau serveur
         console.log(response.status, response.statusText);
     }
 
@@ -336,53 +189,4 @@ async function envoiepokemon2(valsel) {
         // Handle errors
         console.log(response.status, response.statusText);
     }
-}
-
-
-
-async function lancercombat(givenname, vitesse) {
-    const loc = location.origin;// Avoir l'adresse du site sans /
-    const socket = io(`${loc}`);// Connexion au serveur
-    var input = document.getElementById('inputroom');
-    var username = document.getElementById('usernames').innerText;
-    socket.emit("lauchcombat", input.value, username, givenname, vitesse)
-
-}
-function lacmbbtn(nompokemon) {
-    const loc = location.origin;// Avoir l'adresse du site sans /
-    const socket = io(`${loc}`);// Connexion au serveur
-    var input = document.getElementById('inputroom');
-    var username = document.getElementById('usernames').innerText;
-    socket.emit('btncmbserv', input.value, username,nompokemon);
-}
-
-async function puiss() {
-// Puissance = HH × BP × IT × CHG × MS × WS × UA × FA
-/*
-HH (Helping Hand) : 1,5 si le lanceur a reçu l'effet de Coud'main dans le tour ; 1 dans les autres cas.
-
-BP (Base Power) : La puissance de base de l'attaque, que vous pouvez aisément récupérer dans le Pokédex du site ou bien tout simplement dans le descriptif de votre Pokémon sur votre jeu.
-
-IT (Item) : Le modificateur provoqué par l'objet tenu. Vous trouverez une liste des objets affectant la puissance de l'attaque ici.
-
-CHG (Chargeur) : 2 si le Pokémon a lancé Chargeur au tour précédent et que l'attaque est de type Electrik ; 1 dans les autres cas.
-
-MS (Mud Sport) : 0,5 si l'un des Pokémon en jeu a lancé Lance-Boue et que l'attaque est de type Electrik ; 1 dans les autres cas.
-
-WS (Water Sport) : 0,5 si l'un des Pokémon en jeu a lancé Tourniquet et que l'attaque est de type Feu ; 1 dans les autres cas.
-
-UA (User Ability) : Le modificateur apporté par la capacité du lanceur (voir ici).
-
-FA (Foe Ability) : Le modificateur apporté par la capacité de l'adversaire (voir ici).
-*/
-
-return Number(1.5 * 100 * 1 * 1 * 1 * 1 * 1 * 1);
-}
-
-async function calculdesdegat(niv,att,def,attsp,defsp,attspdef,defspdef) {
-
-
-    //Dégâts infligés = (((((((Niveau × 2 ÷ 5) + 2) × Puissance × Att[Spé] ÷ 50) ÷ Def[Spé]) × Mod1) + 2) × CC × Mod2 × R ÷ 100) × STAB × Type1 × Type2 × Mod3
-
-
 }
